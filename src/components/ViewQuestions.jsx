f3d7ec926990024b151aa24b52bd779659a894f3
Fabian Duran <ingfabianavellaneda@gmail.com>
2023-05-30 21:02:21 -0500

    Add buttons edit and delete into questions

--- src/components/ViewQuestions.jsx
+++ src/components/ViewQuestions.jsx
@@ -1,24 +1,47 @@
-import { Grid, TextField } from "@mui/material";
+import { Fragment } from "react";
+import { Grid, TextField, IconButton } from "@mui/material";
+import SettingsSuggestIcon from "@mui/icons-material/SettingsSuggest";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export default function ViewQuestions({ questions = [] }) {
   return (
-    <Grid container sx={{ marginTop: 1, padding: 3, border: "1px dashed grey" }}
-      rowSpacing={2}>
+    <Grid container sx={{
+      marginTop: 1,
+      paddingX: 2,
+      paddingBottom: 2,
+      border: "1px dashed grey",
+      borderRadius: 3
+    }}
+      rowSpacing={1}>
       {
         questions.map((question, index) => (
-          <Grid key={index} item md={12}>
-            {
-              question.typeQuestion === "informativo" && (
-                <TextField
-                  fullWidth
-                  label={question.questionText}
-                  value={question.textInformation}
-                  variant="filled"
-                  disabled={true}
-                  multiline />
-              )
-            }
-          </Grid>
+          <Fragment key={index}>
+            <Grid
+              item
+              display="flex"
+              justifyContent="end"
+              md={12}>
+              <IconButton aria-label="Update field" sx={{ color: "#BD77CE" }}>
+                <SettingsSuggestIcon />
+              </IconButton>
+              <IconButton aria-label="Delete field" sx={{ color: "#D14A4A" }}>
+                <DeleteIcon />
+              </IconButton>
+            </Grid>
+            <Grid item md={12}>
+              {
+                question.typeQuestion === "informativo" && (
+                  <TextField
+                    fullWidth
+                    label={question.questionText}
+                    value={question.textInformation}
+                    variant="filled"
+                    disabled={true}
+                    multiline />
+                )
+              }
+            </Grid>
+          </Fragment>
         ))
       }
     </Grid>
