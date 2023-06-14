83f4f75dcd0ce83999cf856bc1c148d3f768be68
Fabian Duran <ingfabianavellaneda@gmail.com>
2023-06-13 19:45:32 -0500

    Install zustand

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,15 @@
-import { Box } from "@mui/material";
+import { Box, Backdrop, CircularProgress } from "@mui/material";
 import NavigationDrawer from "./NavigationDrawer";
 
 export default function Layout({ children }) {
   return (
     <Box sx={{ display: 'flex' }}>
       <NavigationDrawer />
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={false}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
       <Box component="main" sx={{ flexGrow: 1, padding: 5 }}>
         {children}
       </Box>
